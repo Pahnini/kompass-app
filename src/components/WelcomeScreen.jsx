79b92cb2667d4a3e256412b689fa9f2420cab60e
Florian Pahn <florianpahn89@gmail.com>
2025-06-19 14:24:05 +0200

    Fix: WelcomeScreen angepasst

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -7,7 +7,6 @@ export default function WelcomeScreen({ onContinue }) {
     <div className="welcome-screen">
       <div className="welcome-content">
         <img src={compassImg} alt="Kompass Illustration" />
-        <p>Dein digitaler Kompass für den Alltag nach der Klinik</p>
         <button onClick={onContinue}>Los geht's</button>
       </div>
     </div>
